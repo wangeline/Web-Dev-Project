77cb4e6612d6105f30ca31d557cf2b17824a26f1
wangeline <137566286+wangeline@users.noreply.github.com>
2023-06-28 14:55:55 -0700

    Update js2.js

--- js2.js
+++ js2.js
@@ -1,1 +1,21 @@
+let button = document.getElementById("back");
+button.addEventListener("click", function() {         window.location.replace("https://codepen.io/Angie-W/full/oNQBdLw");
+  //https://codepen.io/Angie-W/full/zYMNMPQ (info)
+  //https://codepen.io/Angie-W/full/oNQBdLw (flix)
+});
 
+  function KO() {
+    alert("Knives Out is a 2019 American mystery film written and directed by Rian Johnson, who also serves as a co-producer. It follows master detective Benoit Blanc investigating the death of the patriarch of a wealthy, dysfunctional family.");
+  }
+
+  function THOI() {
+    alert("A shy, straight-A Chinese-American student helps the school jock woo a girl whom, secretly, they both desire. They find themselves connecting and learn about the nature of love.");
+  }
+
+  function EH1() {
+    alert("While searching for her missing mother, intrepid teen Enola Holmes uses her sleuthing skills to outsmart big brother Sherlock and help a runaway lord.");
+  }
+
+function EH2() {
+  alert("Enola Holmes takes on her first case as a detective, but to unravel the mystery of a missing girl, she'll need some help from friends -- and brother Sherlock.");
+}
