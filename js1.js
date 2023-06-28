e10ca2b2ed963d812bf1baa8b6a1d4a90e6160c5
wangeline <137566286+wangeline@users.noreply.github.com>
2023-06-28 14:55:36 -0700

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,1 +1,5 @@
+let image = document.getElementById("pfp");
 
+image.addEventListener("click", function() {          window.location.replace("https://codepen.io/Angie-W/full/zYMNMPQ");
+  //https://codepen.io/Angie-W/full/zYMNMPQ
+});
