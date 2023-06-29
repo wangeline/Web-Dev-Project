71d1eb4062381174f6084f24d7b2ee19942a6bcd
wangeline <137566286+wangeline@users.noreply.github.com>
2023-06-28 18:23:04 -0700

    Update and rename js2.json to js2.js

---
+++ js2.js
@@ -0,0 +1,48 @@
+let button = document.getElementById("back");
+button.addEventListener("click", function() {         window.location.replace("https://codepen.io/Angie-W/full/oNQBdLw");
+  //https://codepen.io/Angie-W/full/zYMNMPQ (info)
+  //https://codepen.io/Angie-W/full/oNQBdLw (flix)
+});
+
+  function KO() {
+    alert("Knives Out is a 2019 American mystery film written and directed by Rian Johnson, who also serves as a co-producer. It follows master detective Benoit Blanc investigating the death of the patriarch of a wealthy, dysfunctional family.");
+  }
+
+  function THOI() {
+    alert("A shy, straight-A Chinese-American student helps the school jock woo a girl whom, secretly, they both desire. They find themselves connecting and learn about the nature of love.");
+  }
+
+  function EH1() {
+    alert("While searching for her missing mother, intrepid teen Enola Holmes uses her sleuthing skills to outsmart big brother Sherlock and help a runaway lord.");
+  }
+
+function EH2() {
+  alert("Enola Holmes takes on her first case as a detective, but to unravel the mystery of a missing girl, she'll need some help from friends -- and brother Sherlock.");
+}
+
+function ST() {
+  alert("In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries.");
+}
+
+function AWAE() {
+  alert("This reimagining of the classic book and film is a coming-of-age story about a young orphan who is seeking love, acceptance and her place in the world. Amybeth McNulty stars as Anne, a 13-year-old who has endured an abusive childhood in orphanages and the homes of strangers. In the late 1890s, Anne is mistakenly sent to live with aging siblings, Marilla and Matthew Cuthbert, who live on Prince Edward Island. Anne, who proves to be uniquely spirited, imaginative and smart, transforms the lives of Marilla, Matthew and everyone else in their small town.");
+}
+
+function bri() {
+  alert("During the Regency era in England, eight close-knit siblings of the powerful Bridgerton family attempt to find love.");
+}
+
+function ES() {
+  alert("It's the 1990s in Boring, Ore., and groups of outcasts in Boring High School's AV and drama clubs are trying to brave the ups and downs of teenage life in a simpler world without smartphones and other 21st century technology. The two crews of nerdy students are frustrated that no one is taking them seriously so they decide the best way to make it through high school is to join forces and make a movie -- that'll help them get noticed. And there's also the usual teenage romance issues to deal with. Luke, the de facto ringleader of the AV club, falls for Kate, the principal's daughter, but she doesn't necessarily reciprocate the romantic feelings Luke has for her and is dealing with her own issues.");
+}
+
+function songRec() {
+  var input = document.getElementById("songIn");
+  alert("Alright! I'll listen to " + input.value + ". Thank you for the recommendation!");
+}
+
+
+function movieRec() {
+  var input = document.getElementById("movieIn");
+  alert("Thank you for the recommendation! I'll be sure to add " + input.value + " to my watchlist.");
+}
